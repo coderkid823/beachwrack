93a396348e661eb9d36fba3c5c2b8dbfed7a78d6
Ethan L <67442202+coderkid823@users.noreply.github.com>
2020-07-28 15:56:03 -0700

    Update main.R
    This is basically my code for the wrack data. Lines 23 to 27 I edit frequently to fit the graph I want to choose. "wrackData" is the variable name I use to store the read.csv file. I installed ggplot2 and viridis for this.

--- main.R
+++ main.R
@@ -1,1 +1,32 @@
 #R Code?
+
+
+wrackData <- read.csv(file.choose())
+head(wrackData)
+ls(wrackData)
+
+library(ggplot2)
+library(viridis)
+###################
+# GGPLOT SCRIPT
+
+
+g <- ggplot(wrackData, aes(x=YEAR, y=BIOMASS)) + geom_point() + geom_smooth(method="lm")
+plot(g)
+g + ggtitle("Year x Biomass", subtitle="Using ggplot2") + xlab("YEAR") + ylab("BIOMASS")
+#How to compare a character and numeric?
+
+
+head(wrackData)
+ls(wrackData)
+ggplot(wrackData, aes(YEAR, SITE))+
+  geom_point(aes(color = SITE)) +
+ # geom_smooth(aes(color = TAXON_SPECIES, fill = TAXON_SPECIES), method = "lm") + 
+  scale_color_viridis(discrete = TRUE, option = "D")+
+  scale_fill_viridis(discrete = TRUE) +
+  ggtitle("Year x SITE", subtitle="With the \"VIRIDIS\" package")
+
+
+######
+wrackType <- ggplot(data=wrackData, aes(x=factor(wrackData$TYPE)))
+wrackType
